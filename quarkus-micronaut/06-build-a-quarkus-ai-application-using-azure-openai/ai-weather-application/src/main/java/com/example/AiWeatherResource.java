7f9ba026e5f709ae60f5cb9e522490f928f201cd
Jianguo Ma <jiangma@microsoft.com>
2024-06-26 07:38:31 +0800

    06-build-a-quarkus-ai-application-using-azure-openai: doc
    Signed-off-by: Jianguo Ma <jiangma@microsoft.com>

--- quarkus-micronaut/06-build-a-quarkus-ai-application-using-azure-openai/ai-weather-application/src/main/java/com/example/AiWeatherResource.java
+++ quarkus-micronaut/06-build-a-quarkus-ai-application-using-azure-openai/ai-weather-application/src/main/java/com/example/AiWeatherResource.java
@@ -5,16 +5,12 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-import org.slf4j.Logger;
 
 import java.io.IOException;
 import java.time.LocalDate;
-import java.util.List;
 
 @Path("/weather")
 public class AiWeatherResource {
-
-    private static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(AiWeatherResource.class);
 
     private final AiWeatherService aiWeatherService;
     private final String weatherHistory;
@@ -30,16 +26,12 @@ public class AiWeatherResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String askAi(String question) {
 
-        LOGGER.info("\uD83D\uDCDD Preparing response...");
-
         String result = aiWeatherService.ask(
             LocalDate.now().toString(),
             "Paris",
             weatherHistory,
             question
         );
-
-        LOGGER.info("✅ Response for \"{}\" ready", question);
 
         return result;
     }
